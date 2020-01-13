3f0cbb2dd87e7493717fc1f01701cf01f7c79695
prashantgaur895 <rs8011905@gmail.com>
2020-01-13 18:11:04 +0530

    new file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, Devops Class");
+    System.out.println("Hello, Hello All");
+    System.out.println("Hello, My Friends");
+    System.out.println("Hello, All Guyz from Hyderabad");
+    System.out.println("Hello ALL, Learn Linux Adminstration");
+    System.out.println("Hello All, Linux is very much needed for Devops");
+  }
+}
